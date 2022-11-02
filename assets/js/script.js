008c527847e67d1235e08de0a1cc706fe9cf536c
Gypsy Bird <110423762+hcruz77@users.noreply.github.com>
2022-11-02 17:49:37 -0400

    started html over, fixed save functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,21 @@
 var startBtn = document.getElementById('startBtn');
 var questionHeader = document.getElementById('questionHeader');
+var scores = document.getElementById('scores');
 var buttonArea = document.getElementById('buttonArea');
 var game = document.getElementById('game');
 var intro = document.getElementById('intro');
 var endScreenEl = document.getElementById('endScreen');
 var saveScoreBtn = document.getElementById('saveScore');
 var timerEl = document.getElementById('timeEl');
+var result = document.getElementById('result');
+
 
 var btnA = document.getElementById('a');
 var btnB = document.getElementById('b');
 var btnC = document.getElementById('c');
-var btnD = document.getElementById('d');
+
+var scoreCounter = 0;
+
 
 //array for question and answer choices
 const questions = [
@@ -25,8 +30,8 @@ const questions = [
     question: 'What is a function?',
     choices: ['A fun event with snacks',
       'The most popular programming language',
-      'Reusable blocks of code'],
-    answer: 'Reusable blocks of code'
+      'Blocks of code designed to perform a particular task'],
+    answer: 'Blocks of code designed to perform a particular task'
   },
   {
     question: 'What is a variable?',
@@ -48,7 +53,9 @@ const questions = [
 
 let arrayIndex = 0;
 let scoresArr;
-let timeLeft = 30;
+let timeLeft = 20;
+
+
 
 function startGame() {
   game.classList = '';
@@ -56,32 +63,7 @@ function startGame() {
 
   displayQuestions();
 
-  var timeInterval = setInterval(function () {
-    // As long as the `timeLeft` is greater than 1
-    if (arrayIndex > 3) {
-      clearInterval(timeInterval);
-      endScreen();
-    } else {
-      if (timeLeft > 1) {
-        // Set the `textContent` of `timerEl` to show the remaining seconds
-        timerEl.textContent = timeLeft + ' seconds remaining';
-        // Decrement `timeLeft` by 1
-        timeLeft--;
-      } else if (timeLeft === 1) {
-        // When `timeLeft` is equal to 1, rename to 'second' instead of 'seconds'
-        timerEl.textContent = timeLeft + ' second remaining';
-        timeLeft--;
-      } else {
-        // Once `timeLeft` gets to 0, set `timerEl` to an empty string
-        timerEl.textContent = '';
-        // Use `clearInterval()` to stop the timer
-        clearInterval(timeInterval);
-        // Call the `displayMessage()` function
-        endScreen();
-      }
-    }
-  }, 1000);
-}
+
 //displays questions and choices
 function displayQuestions() {
   questionHeader.textContent = questions[arrayIndex].question;
@@ -89,13 +71,68 @@ function displayQuestions() {
   btnA.textContent = questions[arrayIndex].choices[0];
   btnB.textContent = questions[arrayIndex].choices[1];
   btnC.textContent = questions[arrayIndex].choices[2];
-  btnD.textContent = questions[arrayIndex].choices[3];
+
 };
-//clears the screen
+//clears and displays new set of questions 
 function endScreen() {
   game.classList = 'hide';
   endScreenEl.classList = '';
 }
+
+
+//alerts user of answer results and when game is over
+
+buttonArea.addEventListener('click', function (event) {
+  console.log(event.target.textContent);
+
+  if (arrayIndex >= 3) {
+    console.log("Game is done");
+    result.textContent = "GAME IS DONE"
+    endScreen();
+  } else {
+    if (event.target.textContent === questions[arrayIndex].answer) {
+      console.log('Correct');
+      result.textContent = "Correct!"
+
+      arrayIndex++;
+      displayQuestions();
+    } else {
+      console.log('Incorrect!');
+      result.textContent = "incorrect!"
+      timeLeft -= 5;
+      arrayIndex++;
+      displayQuestions();
+    }
+  }
+});
+
+//timer function
+var timeInterval = setInterval(function () {
+  // As long as the `timeLeft` is greater than 1
+  if (arrayIndex > 3) {
+    clearInterval(timeInterval);
+    endScreen();
+  } else {
+    if (timeLeft > 1) {
+      // Set the `textContent` of `timerEl` to show the remaining seconds
+      timerEl.textContent = timeLeft + ' seconds remaining';
+      // Decrement `timeLeft` by 1
+      timeLeft--;
+    } else if (timeLeft === 1) {
+      // When `timeLeft` is equal to 1, rename to 'second' instead of 'seconds'
+      timerEl.textContent = timeLeft + ' second remaining';
+      timeLeft--;
+    } else {
+      // Once `timeLeft` gets to 0, set `timerEl` to an empty string
+      timerEl.textContent = '';
+      clearInterval(timeInterval);
+   
+      endScreen();
+    }
+  }
+}, 1000);
+}
+
 //saves score and initials to local storage
 function saveScore() {
   var initialsEl = document.getElementById('initials').value;
@@ -109,28 +146,11 @@ function saveScore() {
   scoresArr.push(initialsEl);
 
   localStorage.setItem('Scores', scoresArr);
+ 
 }
 
-//executes the button function when user chooses an answer
-buttonArea.addEventListener('click', function (event) {
-  console.log(event.target.textContent);
-
-  if (arrayIndex >= 3) {
-    console.log("Game is done");
-    endScreen();
-  } else {
-    if (event.target.textContent === questions[arrayIndex].answer) {
-      console.log('Correct');
-      arrayIndex++;
-      displayQuestions();
-    } else {
-      console.log('Incorrect!');
-      timeLeft -= 5;
-      arrayIndex++;
-      displayQuestions();
-    }
-  }
-});
 
 startBtn.addEventListener('click', startGame);
 saveScoreBtn.addEventListener('click', saveScore);
+
+
